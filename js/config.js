89a1a35f87794678c27861325a8d7e6bdc45085a
cozinhadagabriela-stack <cozinhadagabriela@gmail.com>
2026-03-25 14:24:40 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+export const PLATFORM_CONFIG = {
+  appName: 'Portal de Vendas Cozinha da Gabriela',
+  maxActiveVendors: 20,
+  defaultCatalog: [
+    'Biscoito de coco',
+    'Biscoito de goiabada',
+    'Sequilho de limão',
+    'Sequilho de leite condensado',
+    'Bolacha de polvilho doce'
+  ]
+};
