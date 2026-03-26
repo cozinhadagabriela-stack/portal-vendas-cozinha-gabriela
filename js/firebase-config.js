27392a7e93dcf33418d3068dd6430fa3ca04eaa5
cozinhadagabriela-stack <cozinhadagabriela@gmail.com>
2026-03-26 14:18:16 -0400

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC-NztsdPM44KD8q_reaFKQjlfButyz85Q',
+  authDomain: 'portal-vendas-coz-da-gabriela.firebaseapp.com',
+  projectId: 'portal-vendas-coz-da-gabriela',
+  storageBucket: 'portal-vendas-coz-da-gabriela.firebasestorage.app',
+  messagingSenderId: '868952197423',
+  appId: '1:868952197423:web:b21fe7595904cead9eb9da'
+};
